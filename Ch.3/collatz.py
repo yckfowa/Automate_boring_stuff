cbeb260a03518394257b47de7c23786a203daa58
yckfowa <80372122+yckfowa@users.noreply.github.com>
2021-07-28 12:29:48 +0800

    commits

--- Ch.3/collatz.py
+++ Ch.3/collatz.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-def collatz(number):
-    if number % 2 == 0:
-        print(number//2)
-        return number//2
-    elif number % 2 == 1:
-        print(3*number+1)
-        return 3*number+1
-
-
-try:
-    n = int(input("Enter a number: "))
-    while n != 1 and n > 0:
-        n = collatz(n)
-except ValueError:
-    print("Please enter a (positive) integer instead of string.. program exits...")
-
-=======
 
 def collatz(number):
     if number % 2 == 0:
@@ -33,4 +15,3 @@ try:
 except ValueError:
     print("Please enter a (positive) integer instead of string.. program exits...")
 
->>>>>>> b6635c9564d6f61fc50c0b2f94c066a5e5a58140
